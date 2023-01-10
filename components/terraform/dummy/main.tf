e5e0035bfcab057e47cef8bee1d6895c09ab30fb
Matt Calhoun <matt@cloudposse.com>
2023-01-10 12:00:23 -0500

    test affected

--- components/terraform/dummy/main.tf
+++ components/terraform/dummy/main.tf
@@ -5,7 +5,7 @@ variable "seed" {
 resource "random_id" "server" {
   keepers = {
     # Generate a new id each time we switch to a new AMI id
-    seed = var.seed
+    seed = "${var.seed}1"
   }
 
   byte_length = 8
